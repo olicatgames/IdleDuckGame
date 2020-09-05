8db32f64b4e79df01b63967d700a7b03fcde4879
Protoyon <tommywlewis3142@gmail.com>
2020-09-05 05:44:56 -0400

    Version 0.0.4
    Upgrade Menu First Version
    
    Co-Authored-By: Daub0 <64547159+Daub0@users.noreply.github.com>

---
+++ MobileGameProject/Assets/Scripts/openUpgradeMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class openUpgradeMenu : MonoBehaviour
+{
+
+    private Button upgradeButton;
+    public GameObject upgradeMenu;
+    private bool isOpen;
+
+    private AudioSource beep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        upgradeButton = this.GetComponent<Button>();
+        upgradeButton.onClick.AddListener(OpenUpgrades);
+
+        beep = this.GetComponent<AudioSource>();
+    }
+
+    void OpenUpgrades()
+    {
+        if (!isOpen)
+        {
+            upgradeMenu.SetActive(true);
+            beep.pitch = 1;
+            beep.Play();
+            isOpen = true;
+        }
+        else
+        {
+            upgradeMenu.SetActive(false);
+            beep.pitch = 0.9f;
+            beep.Play();
+            isOpen = false;
+        }
+    }
+
+}
