77a0f5eed29693ebad429bd837d64e5b04639c04
Protoyon <tommywlewis3142@gmail.com>
2020-09-05 04:48:21 -0400

    Version 0.0.3
    Added Duck Functionality
    -Animations
    -Money
    -Noises
    Updated Button Sprites (Daub0)
    -Upgrades
    -Stats
    -Health
    Adjusted Background
    -Grass
    -Ground
    
    Co-Authored-By: Daub0 <64547159+Daub0@users.noreply.github.com>

---
+++ MobileGameProject/Assets/Scripts/mainDuck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainDuck : MonoBehaviour
+{
+
+    private Button duck;
+    public Text moneyCount;
+
+    public float money;
+    public float onClickMoney;
+
+    private AudioSource quack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quack = this.GetComponent<AudioSource>();
+        duck = this.GetComponent<Button>();
+        duck.onClick.AddListener(DuckClick);
+    }
+
+    // Update is called once per frame
+    void DuckClick()
+    {
+        Debug.Log("Click");
+        money = money + onClickMoney;
+        quack.Play();
+
+
+        moneyCount.text = "$" + money.ToString("F2");
+    }
+}
